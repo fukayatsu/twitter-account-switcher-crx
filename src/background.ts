97957db3e537a8c3be3c04806d227a7cee3d50a1
fukayatsu <fukayatsu@gmail.com>
2024-07-31 15:24:41 +0900

    Use chrome.storage.session instead of localStorage

--- src/background.ts
+++ src/background.ts
@@ -10,3 +10,19 @@ chrome.action.onClicked.addListener((tab) => {
     event: "browserActionOnClicked",
   });
 });
+
+chrome.runtime.onMessage.addListener((message, sender) => {
+  if (message.event == "save_url") {
+    chrome.storage.session.set({ url: message.url });
+  } else if (message.event == "restore_url") {
+    chrome.storage.session.get("url", (data) => {
+      if (data.url) {
+        chrome.tabs.sendMessage(sender.tab.id, {
+          event: "goto_url",
+          url: data.url,
+        });
+        chrome.storage.session.remove("url");
+      }
+    });
+  }
+});
--- src/content.ts
+++ src/content.ts
@@ -1,51 +1,45 @@
-const key = "tas_return_to_url";
+chrome.runtime.sendMessage({ event: "restore_url" });
 
-if (localStorage[key]) {
-  const url = localStorage[key];
-  delete localStorage[key];
-  location.href = url;
-}
+chrome.runtime.onMessage.addListener(async (message) => {
+  if (message.event == "goto_url") {
+    location.href = message.url;
+  } else if (message.event == "browserActionOnClicked") {
+    chrome.runtime.sendMessage({ event: "save_url", url: location.href });
 
-chrome.runtime.onMessage.addListener((message) => {
-  if (message.event != "browserActionOnClicked") {
-    return;
-  }
-
-  localStorage[key] = location.href;
-
-  let accountSwitcherButton = document.querySelector(
-    '[data-testid="SideNav_AccountSwitcher_Button"]',
-  ) as HTMLElement;
-
-  if (!accountSwitcherButton) {
-    accountSwitcherButton = document.querySelector(
-      'nav[role="navigation"] div[role="button"]',
-    ) as HTMLElement;
-  }
-
-  if (!accountSwitcherButton) {
-    alert("Error: AccountSwitcher Button not found.");
-    return;
-  }
-
-  if (!document.querySelector("[data-testid='HoverCard']")) {
-    accountSwitcherButton.click();
-  }
-
-  setTimeout(() => {
-    let currentAccount = document.querySelector(
-      "[data-testid='HoverCard'] li[data-testid='UserCell']",
+    let accountSwitcherButton = document.querySelector(
+      '[data-testid="SideNav_AccountSwitcher_Button"]',
     ) as HTMLElement;
 
-    let nextAccount;
-
-    if (currentAccount) {
-      nextAccount = currentAccount.nextSibling as HTMLElement;
-      if (!nextAccount) {
-        alert("Error(1): Anothor account not found.");
-        return;
-      }
+    if (!accountSwitcherButton) {
+      accountSwitcherButton = document.querySelector(
+        'nav[role="navigation"] div[role="button"]',
+      ) as HTMLElement;
     }
-    nextAccount.click();
-  }, 500);
+
+    if (!accountSwitcherButton) {
+      alert("Error: AccountSwitcher Button not found.");
+      return;
+    }
+
+    if (!document.querySelector("[data-testid='HoverCard']")) {
+      accountSwitcherButton.click();
+    }
+
+    setTimeout(() => {
+      let currentAccount = document.querySelector(
+        "[data-testid='HoverCard'] li[data-testid='UserCell']",
+      ) as HTMLElement;
+
+      let nextAccount;
+
+      if (currentAccount) {
+        nextAccount = currentAccount.nextSibling as HTMLElement;
+        if (!nextAccount) {
+          alert("Error(1): Anothor account not found.");
+          return;
+        }
+      }
+      nextAccount.click();
+    }, 500);
+  }
 });
